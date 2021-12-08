226ef2b7f5fbd05d97886659280bc30b53bf3de7
Michael Yuanhao Deng <54728835+yuanhaomichael@users.noreply.github.com>
2021-12-08 08:48:32 -0500

    tests

--- test/TodoList.Test.js
+++ test/TodoList.Test.js
@@ -1,4 +1,7 @@
+// testing smart contract 
 // require the smart contract we created. Artifacts is truffle's version of contracts
+
+const { assert } = require("chai")
 
 const TodoList = artifacts.require('./TodoList.sol')
 
@@ -7,5 +10,22 @@ contract('TodoList', (accounts)=>{
     //so before each test runs, have the deployed version of TodoList be saved to todoList
     before(async() => {
         this.todoList = await TodoList.deployed()
+    })
+     
+    // The it() function is defined by the jasmine testing framework
+    it('deployed successfully', async () => {
+        const address =  await this.todoList.address
+        assert.notEqual(address, 0x0)
+        assert.notEqual(address, null)
+        assert.notEqual(address, undefined)
+        assert.notEqual(address, '')
+    })
+
+    it('lists tasks', async () => {
+        const taskCount =  await this.todoList.taskCount
+        const task =  await this.todoList.tasks(taskCount)
+        // see if the version of data on the chain is equal to what we think it is
+        assert.equal(task.id.toNumber(), taskCount.toNumber())
+        assert.equal(task.completed, false)
     })
 })
